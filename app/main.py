f44ce7a1f007a2830f46ffa1a80869e27474f2c3
0lex1y <kotov.oleksiy@icloud.com>
2025-09-08 00:52:27 +0300

    DD coords

--- app/main.py
+++ app/main.py
@@ -48,8 +48,8 @@ class FlyingRobot(BaseRobot):
 class DeliveryDrone(FlyingRobot):
     def __init__(self, name: str,
                  weight: int,
-                 coords: list[int] | None,
-                 max_load_weight: int,
+                 coords: list[int] | None = None,
+                 max_load_weight: int = 0,
                  current_load: Cargo | None = None) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
